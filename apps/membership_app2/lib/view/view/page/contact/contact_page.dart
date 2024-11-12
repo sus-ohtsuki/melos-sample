2c05cb9688a697e914fd25e02a9ed9acfe599630
sus.ohtsuki <s-ohtsuki@s-ucs.com>
2024-11-13 04:01:32 +0900

    feat: app1 / app2 bottom navigation page frames

---
+++ apps/membership_app2/lib/view/view/page/contact/contact_page.dart
@@ -0,0 +1,17 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Contact Page',
+        ),
+      ),
+    );
+  }
+}
